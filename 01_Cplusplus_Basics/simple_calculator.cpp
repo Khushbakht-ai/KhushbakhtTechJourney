25ff7a5ba98e9c6d15a5fc80f02e8de82aa18a5b
Khushbakht-ai <khushi.ai.s19@gmail.com>
2025-06-11 14:13:13 +0500

    Created in Oct 2024 - Simple calculator using switch

---
+++ 01_Cplusplus_Basics/simple_calculator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+    char op;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    switch(op) {
+        case '+': cout << a + b; break;
+        case '-': cout << a - b; break;
+        case '*': cout << a * b; break;
+        case '/': cout << a / b; break;
+        default: cout << "Invalid operator!";
+    }
+    return 0;
+}
